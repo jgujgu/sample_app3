07be923d6ac31046dc818fbeeaf2ae12100b9d5f
jgu2160 <jgu@jeffreygu.com>
2013-12-02 21:13:06 -0700

    fixed problems

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -7,13 +7,8 @@ Pony.options = {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-<<<<<<< HEAD
     :user_name            => 'jgujgujgu',
     :password             => '5493554935',
-=======
-    :user_name            => 'denverpiggyback',
-    :password             => 'tayedrums1',
->>>>>>> d91b771614447ff9e134bbb0c63e9d34a763844a
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
   }
