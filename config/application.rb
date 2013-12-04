ce23e233076a5c7ab1bc1d9b9f99ba05768abc84
jgu2160 <jgu@jeffreygu.com>
2013-12-03 20:54:32 -0700

    fixed precompile problem

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,7 @@ module JguPage
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile =false
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.initialize_on_precompile = false
   end
 end
